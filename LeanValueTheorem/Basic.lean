92f42028ab2ab6c2f60490c2349b5df49023fbcb
qw04 <shahsaumya2004@gmail.com>
2025-11-04 21:32:17 +0000

    some comments about what exactly to formalize

--- LeanValueTheorem/Basic.lean
+++ LeanValueTheorem/Basic.lean
@@ -1,1 +1,21 @@
 def hello := "world"
+
+-- define closed and open intervals
+
+-- Define Limits for Sequences
+-- Algebra of Limits for Sequences (lemma)
+-- Limits of non +ve is not +ve (lemma)
+-- Limits of non -ve is not -ve (lemma)
+-- Definte Limits for Functions
+-- Algebra of Limits for Functions(lemma)
+-- ε/δ Continuity Definition
+-- Sequential Continuity Definition
+-- Equivalence of Both Continuity Definitions (lemma)
+-- Algebra of Continuous Functions (lemma)
+
+
+-- Attainment of Max for Continuous Functions on [a, b]
+-- Definiton of Derivative of f at c
+-- Functions with zero derivatives (lemma)
+-- Rolles Theorem (Theorem)
+-- Mean Value Theorem (Theorem)
