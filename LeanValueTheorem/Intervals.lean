72acaa29434300e89670f2fbff6abb8c779fd1b4
beanrob <contact.beanrob@gmail.com>
2025-11-11 13:44:36 +0000

    added proof that open intervals are intervals, closed intervals are intervals

--- LeanValueTheorem/Intervals.lean
+++ LeanValueTheorem/Intervals.lean
@@ -6,8 +6,38 @@ def cci : ℝ → ℝ → Set ℝ := (fun a b => { x : ℝ | a ≤ x ∧ x ≤ b
 def oci : ℝ → ℝ → Set ℝ := (fun a b => { x : ℝ | a < x ∧ x ≤ b })
 def coi : ℝ → ℝ → Set ℝ := (fun a b => { x : ℝ | a ≤ x ∧ x < b })
 
-def is_interval (I : Set ℝ) : Prop := ∃ a b : ℝ,
+def is_interval (I : Set ℝ) : Prop :=
+  ∃ a b : ℝ,
   I = ooi a b ∨
   I = cci a b ∨
   I = oci a b ∨
   I = coi a b
+
+def is_open (I : Set ℝ) : Prop :=
+  ∃ a b : ℝ,
+  I = ooi a b
+
+def is_closed (I : Set ℝ) : Prop :=
+  ∃ a b : ℝ,
+  I = cci a b
+
+lemma open_interval (I : Set ℝ) : is_open I → is_interval I := by
+  unfold is_open
+  intro h
+  let ⟨a, b, h'⟩ := h
+  unfold is_interval
+  use a
+  use b
+  aesop
+
+lemma closed_interval (I : Set ℝ) : is_closed I → is_interval I := by
+  unfold is_closed
+  intro h
+  let ⟨a, b, h'⟩ := h
+  unfold is_interval
+  use a
+  use b
+  aesop
+  
+example : is_interval (ooi 0 1) := by
+  exact
