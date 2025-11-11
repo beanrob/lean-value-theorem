0f8a6335e13939f276aba1ecf0115fe029121403
beanrob <contact.beanrob@gmail.com>
2025-11-11 13:00:17 +0000

    added dummy definitions for continuous

---
+++ LeanValueTheorem/Cont.lean
@@ -0,0 +1,7 @@
+import Mathlib.Data.Real.Basic
+
+def is_interval (I : Set ℝ) : Prop := True
+
+def is_cont_at (is_interval I : Set ℝ) (f: I → ℝ) (a : I) : Prop := True
+
+def is_cont (is_interval I : Set ℝ) (f: I → ℝ) : Prop := True
