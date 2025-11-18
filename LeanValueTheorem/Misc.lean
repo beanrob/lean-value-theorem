93486f97f46e5b2446862587df9c2315fa2aca5a
qw04 <shahsaumya2004@gmail.com>
2025-11-18 13:46:52 +0000

    filled in Misc file things

--- LeanValueTheorem/Misc.lean
+++ LeanValueTheorem/Misc.lean
@@ -5,6 +5,9 @@ import LeanValueTheorem.Intervals
 def is_const_fun (I : Set ℝ) (f : I → ℝ) : Prop :=
   ∀ x y : I, f x = f y
 
--- Definition for a being a 
-def is_fun_min (I : Set ℝ) (hI : is_interval I) (f : I → ℝ) (a : ℝ) : Prop := sorry
-def is_fun_max (I : Set ℝ) (hI : is_interval I) (f : I → ℝ) (b : ℝ) : Prop := sorry
+-- Definition for a being a
+def is_fun_min (I : Set ℝ) (hI : is_interval I) (f : I → ℝ) (a : I) : Prop :=
+  ∀ x : I, f a <= f x
+
+def is_fun_max (I : Set ℝ) (hI : is_interval I) (f : I → ℝ) (b : I) : Prop :=
+  ∀ x : I, f b >= f x
