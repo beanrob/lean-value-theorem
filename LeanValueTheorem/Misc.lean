bb4404b8074c41d0f59b24b4bb4dda996000a15f
Odysseas <odysseasgabrielatos@gmail.com>
2025-11-20 16:28:53 +0000

    definitions work for f defined on subset of reals

--- LeanValueTheorem/Misc.lean
+++ LeanValueTheorem/Misc.lean
@@ -2,9 +2,9 @@ import LeanValueTheorem.Intervals
 
 
 -- Definition for f being a constant function
-def is_const_fun (I : Set ℝ) (f : I → ℝ) : Prop :=
+def is_const_fun (I : Set ℝ) (f : ℝ → ℝ) : Prop :=
   ∀ x y : I, f x = f y
 
--- Definition for a being a 
+-- Definition for a being a
 def is_fun_min (I : Set ℝ) (hI : is_interval I) (f : I → ℝ) (a : ℝ) : Prop := sorry
 def is_fun_max (I : Set ℝ) (hI : is_interval I) (f : I → ℝ) (b : ℝ) : Prop := sorry
