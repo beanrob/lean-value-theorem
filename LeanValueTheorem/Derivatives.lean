f905e164aeb32f91e11444b10ea70bbe6817578a
Odysseas <odysseasgabrielatos@gmail.com>
2025-11-21 15:39:14 +0000

    proof that constant function has zero derivative

--- LeanValueTheorem/Derivatives.lean
+++ LeanValueTheorem/Derivatives.lean
@@ -13,8 +13,26 @@ def is_deriv_at (D : Set ℝ) (f : ℝ → ℝ) (f' : ℝ → ℝ) (a : ℝ) : P
 def is_deriv (D : Set ℝ) (f : ℝ → ℝ) (f' : ℝ → ℝ) (A : Set ℝ) : Prop :=
   ∀ a ∈ A, is_deriv_at D f f' a
 
--- Proof that a function has zero derivative if and only if it is constant
+-- Proof that f : D → ℝ has zero derivative if and only if it is constant
 lemma fun_with_zero_deriv
-  (I : Set ℝ) (hI : is_interval I)
-  (f : ℝ → ℝ) :
-  is_deriv I f 0 I ↔ is_const_fun I f := sorry
+  (D : Set ℝ) (f : ℝ → ℝ) :
+  is_deriv D f 0 D ↔ is_const_fun D f := by
+    constructor
+    · intro hder x y hD
+      obtain ⟨hx, hy⟩ := hD
+      sorry --I belive this direction requires the Mean Value Theorem
+    · intro hcon a ha _ ε hε
+      use 1
+      constructor
+      · simp
+      · intro h hh1 hh2
+        simp
+        specialize hcon (a + h) a
+        have hah : a + h ∈ D ∧ a ∈ D := by
+          constructor
+          · exact hh1
+          · exact ha
+        specialize hcon hah
+        rw [hcon]
+        simp
+        exact hε
