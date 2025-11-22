3753e1d3d15df5866e3867a6aca40806e19861aa
beanrob <contact.beanrob@gmail.com>
2025-11-22 22:01:06 +0000

    start proving algebra of limits

--- LeanValueTheorem/Cont.lean
+++ LeanValueTheorem/Cont.lean
@@ -10,7 +10,7 @@ def is_cont_at_ε_δ {I : Set ℝ} (f : I → ℝ) (a : I) : Prop :=
 
 -- Definition for a function being continuous at one point, using sequences
 def is_cont_at_seq {I : Set ℝ} (f : I → ℝ) (a : I) : Prop :=
-  ∀ seq : ℕ → ℝ, is_lim_seq seq a → is_lim_seq (f ∘ seq) (f a)
+  ∀ seq : ℕ → I, is_lim_seq_rest seq a → is_lim_seq (f ∘ seq) (f a)
 
 -- Definition for a function being continuous at a point, using interchangability
 -- of the sequential and ε-δ definitions
@@ -21,13 +21,38 @@ def is_cont_at {I : Set ℝ} (f : I → ℝ) (a : I) : Prop :=
 def is_cont (I : Set ℝ) (hI : is_interval I) (f : I → ℝ) : Prop :=
   ∀ a : I, is_cont_at f a
 
+-- Interchangability of ε-δ definition and sequential
+-- Forwards direction
+lemma cont_ε_δ_imp_cont_seq
+  (I : Set ℝ)
+  (f : I → ℝ)
+  (a : I)
+  {hfIa : is_cont_at_ε_δ f a} :
+  is_cont_at_seq f a := sorry
+-- Backwards direction
+lemma cont_seq_imp_cont_ε_δ
+  (I : Set ℝ)
+  (f : I → ℝ)
+  (a : I)
+  {hfIa : is_cont_at_seq f a} :
+  is_cont_at_ε_δ f a := sorry
+
 -- Algebra of continuous functions (for sums, products, and quotients)
 lemma cont_sum
   (I : Set ℝ) (hI : is_interval I)
   (f g : I → ℝ) (a : I)
   {hfIa : is_cont_at f a}
   {hgIa : is_cont_at g a} :
-  (is_cont_at (fun x => f x + g x) a) := sorry
+  (is_cont_at (fun x => f x + g x) a) := by
+    unfold is_cont_at at hfIa hgIa
+    obtain ⟨hf1, hf2⟩ := hfIa
+    obtain ⟨hg1, hg2⟩ := hgIa
+    unfold is_cont_at_seq at hf2 hg2
+    unfold is_cont_at
+    have seq_cont : is_cont_at_seq (fun x => f x + g x) a := by
+      sorry
+    sorry
+  -- Need to know how to unfold assumptions and give things names
 lemma cont_prod
   (I : Set ℝ) (hI : is_interval I)
   (f g : I → ℝ) (a : I)
@@ -40,20 +65,6 @@ lemma cont_quot
   {hfIa : is_cont_at f a}
   {hgIa : is_cont_at g a} :
   (is_cont_at (fun x => f x / g x) a) := sorry
-
--- Interchangability of ε-δ definition and sequential
--- Forwards direction
-lemma cont_ε_δ_imp_cont_seq
-  (I : Set ℝ) (hI : is_interval I)
-  (f : I → ℝ) (a : I)
-  {hfIa : is_cont_at_ε_δ f a} :
-  is_cont_at_seq f a := sorry
--- Backwards direction
-lemma cont_seq_imp_cont_ε_δ
-  (I : Set ℝ) (hI : is_interval I)
-  (f : I → ℝ) (a : I)
-  {hfIa : is_cont_at_seq f a} :
-  is_cont_at_ε_δ f a := sorry
 
 -- Proof that continuous functions attain their bounds
 lemma cont_attains_bounds
