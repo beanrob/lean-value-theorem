2a0224940662368c87086cdaa9a12357632a346f
chrislev72 <chrislevesley72@gmail.com>
2025-11-25 12:36:03 +0000

    pull

--- LeanValueTheorem.lean
+++ LeanValueTheorem.lean
@@ -3,11 +3,12 @@
 import LeanValueTheorem.Cont
 import LeanValueTheorem.Derivatives
 import LeanValueTheorem.Intervals
+import LeanValueTheorem.Limits
 
-variable {a b : ℝ} {I : Set ℝ} {f f' : ℝ → ℝ}
+variable {a b : ℝ} {f f' : ℝ → ℝ} {I : Set ℝ}
 
-theorem rolle (hI : I = ooi a b) (hfc : is_cont_on_set I hI f) (hff' : is_deriv_on f f' (ooi a b))
+theorem rolle (hfc : is_cont (cci a b) f) (hff' : is_deriv I f f' (ooi a b))
  (hfab : f a = f b) : ∃ c ∈ ooi a b, f' c = 0 := sorry
 
-theorem mvt (hfc : is_cont_on_set I hI f) (hff' : is_deriv_on f f' (ooi a b)) :
+theorem mvt (hfc : is_cont (cci a b) f) (hff' : is_deriv I f f' (ooi a b)) :
  ∃ c ∈ ooi a b, f' c = (f a - f b) / (b - a) := sorry
