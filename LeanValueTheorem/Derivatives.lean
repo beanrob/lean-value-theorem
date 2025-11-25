617046d08aeddac84aceb170126af58d5b9670d6
Odysseas <odysseasgabrielatos@gmail.com>
2025-11-25 13:31:07 +0000

    sorried chain rule

--- LeanValueTheorem/Derivatives.lean
+++ LeanValueTheorem/Derivatives.lean
@@ -49,3 +49,11 @@ lemma product_rule
   is_deriv (D ∩ E) (fun x => (f x) * (g x))
   (fun x => (f' x) * (g x) + (f x) * (g' x)) (D ∩ E) := by
     sorry
+
+lemma chain_rule
+  (D : Set ℝ) (f : ℝ → ℝ) (f' : ℝ -> ℝ) (hf : is_deriv D f f' D)
+  (E : Set ℝ) (g : ℝ → ℝ) (g' : ℝ -> ℝ) (hg : is_deriv E g g' E)
+  (hdom : ∀ x ∈ D, (f x) ∈ E) :
+  is_deriv D (fun x => g (f x))
+  (fun x => (g' (f x)) * (f' x)) D := by
+    sorry
