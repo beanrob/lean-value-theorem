49e8a9912a567975d7f4d2f528262b7ca4b96d6a
beanrob <contact.beanrob@gmail.com>
2025-12-04 16:20:25 +0000

    removed stuff that is moved to boundedness file

--- LeanValueTheorem/Misc.lean
+++ LeanValueTheorem/Misc.lean
@@ -7,7 +7,3 @@ import Mathlib.Algebra.Order.Group.Unbundled.Abs
 -- Definition for f : D → ℝ being a constant function
 def is_const_fun (D : Set ℝ) (f : ℝ → ℝ) : Prop :=
   ∀ x y : ℝ, x ∈ D ∧ y ∈ D → f x = f y
-
--- Definition for a being a
-def is_fun_min (I : Set ℝ) (f : ℝ → ℝ) (a : ℝ) : Prop := sorry
-def is_fun_max (I : Set ℝ) (f : ℝ → ℝ) (b : ℝ) : Prop := sorry
