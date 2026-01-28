4195f9527df8a8cdf4eb408dcd26ed46a7655c78
qw04 <shahsaumya2004@gmail.com>
2026-01-28 20:47:59 +0000

    cont closed imp bounded proved

--- LeanValueTheorem/Bounds.lean
+++ LeanValueTheorem/Bounds.lean
@@ -56,7 +56,7 @@ lemma weierstrass_criterion (f : ℕ → ℝ) (hf : is_sequence f)
 
 lemma bolanzo_weierstrass (f : ℕ → ℝ) (ha : is_sequence f)
   (hfba : BddAbove (f '' Set.univ)) (hfbb : BddBelow (f '' Set.univ)) :
-  (∃k : ℕ → ℕ, ∃ a : ℝ, is_lim_seq (fun n => f (k n)) a) := by sorry
+  ∃ k : ℕ → ℕ, (StrictMono k ∧ ∃ a : ℝ, is_lim_seq (fun n => f (k n)) a) := by sorry
 
 
 theorem cont_closed_imp_bounded (f : ℝ → ℝ) (a b : ℝ) (hfc : is_cont f (cci a b)) :
@@ -77,32 +77,22 @@ theorem cont_closed_imp_bounded (f : ℝ → ℝ) (a b : ℝ) (hfc : is_cont f (
       rw [←hl] at part
       simp at part
       rcases part with ⟨x, hx, x_prop⟩
-      refine ⟨x, hx, ?_⟩
-      exact lt_of_lt_of_le x_prop (le_abs_self (f x))
+      refine ⟨x, hx, lt_of_lt_of_le x_prop (le_abs_self (f x))⟩
     | inr hr =>
       unfold BddBelow lowerBounds at hr
       simp at hr
       rw [Set.not_nonempty_iff_eq_empty] at hr
       rw [←Set.compl_univ_iff] at hr
-      have part : n ∈ Set.univ := by trivial
+      have part : -n ∈ Set.univ := by trivial
       rw [←hr] at part
       simp at part
       rcases part with ⟨x, hx, x_prop⟩
-      refine ⟨x, hx, ?_⟩
-      have in1 := neg_lt_neg x_prop
-      have in2 := neg_le_abs (f x)
-      by_cases hn : n > 0
-      · have in3 := by simpa only [neg_zero] using neg_lt_neg hn
-        sorry
-      · simp at hn
-        have in3 := by simpa only [neg_zero] using neg_le_neg hn
-        have in4 := lt_of_lt_of_le x_prop hn
-        sorry
+      refine ⟨x, hx, lt_of_lt_of_le (by simpa using (neg_lt_neg x_prop)) (neg_le_abs (f x))⟩
 
   -- get sequence that diverges and converges at the same time
   set g : ℕ → ℝ := fun n => Classical.choose (ex n) with hg
   have g_bounds (n : ℕ) := (Classical.choose_spec (ex n)).1
-  have fx_bounds (n : ℕ) := (Classical.choose_spec (ex n)).2
+  have fx_bounds (n : ℝ) := (Classical.choose_spec (ex n)).2
 
   have hgb : BddAbove (g '' Set.univ) := by
     unfold BddAbove upperBounds
@@ -122,14 +112,10 @@ theorem cont_closed_imp_bounded (f : ℝ → ℝ) (a b : ℝ) (hfc : is_cont f (
     rw [hg]
     exact (g_bounds x).left
 
-  have hbw := bolanzo_weierstrass g (by trivial) hgb hgl
-  set k := Classical.choose hbw with hk
-  set lim := Classical.choose (Classical.choose_spec hbw) with hlim
+  rcases bolanzo_weierstrass g (by trivial) hgb hgl with ⟨k, mono, lim, hgk_lim⟩
 
   -- show the limit of subseqeuence lies in cci a b
   have gk_bounds (n : ℕ) : (fun n ↦ g (k n)) n ∈ cci a b := by simpa using g_bounds (k n)
-  have hgk_lim : is_lim_seq (fun n ↦ g (k n)) lim := by
-    simpa [hk, hlim] using Classical.choose_spec (Classical.choose_spec hbw)
   have gk_lim := sequence_in_closed (fun n => g (k n)) lim a b gk_bounds hgk_lim
 
   -- use continuity to show f ∘ g ∘ k → f
@@ -138,8 +124,25 @@ theorem cont_closed_imp_bounded (f : ℝ → ℝ) (a b : ℝ) (hfc : is_cont f (
   have f_lim := cont_at.right gk_lim (fun n => g (k n)) gk_bounds hgk_lim
   rcases f_lim 1 (by norm_num) with ⟨N, hf_prop⟩
 
-  have := fx_bounds (N+1)
-  sorry
+  -- contradict unboundedness with convergence
+  have nat (x : ℝ) : ∃n : ℕ, x < n := by exact exists_nat_gt x
+  rcases nat (max N (1 + |f lim|)) with ⟨n, hxn⟩
+  have this1 := fx_bounds (k n)
+  have maxN := by simpa only [max_comm] using (le_max_right (1 + |f lim|) N )
+
+  have this2 := lt_of_le_of_lt maxN hxn
+  norm_cast at this2
+  have this3 := hf_prop (n) (le_of_lt this2)
+  simp at this3
+
+  have this4 := abs_sub_le (f (g (k n))) (f lim) 0
+  simp at this4
+  have this5 := le_add_of_le_add_right this4 (le_of_lt this3)
+  have maxf := by simpa only [max_comm] using (le_max_left (1 + |f lim|) N )
+
+  have hn_to_hkn : (n : ℝ) ≤ (k n : ℝ) := by exact_mod_cast (mono.id_le n)
+  have this6 := lt_trans (lt_of_lt_of_le (lt_of_le_of_lt maxf hxn) hn_to_hkn) this1
+  exact (not_lt_of_ge this5) this6
 
 
 theorem cont_closed_attains_bounds (f : ℝ → ℝ) (a b : ℝ) (cont : is_cont f (cci a b)) :
